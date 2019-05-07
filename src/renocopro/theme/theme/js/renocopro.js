b3bb6da5acbbeafad1d3f40775f013fd0c75e400
AuroreMariscal <aurore@affinitic.be>
2019-05-07 19:39:10 +0200

    Add style

--- src/renocopro/theme/theme/js/renocopro.js
+++ src/renocopro/theme/theme/js/renocopro.js
@@ -6,7 +6,8 @@
  */
 
 $(document).ready(function() {
-  /* ----  SLIDER HOSTING TILE START ---- */
+
+  /* ----  SLIDER TILE GLOBAL START ---- */
     var $window = $(window)
     var flexslider = { vars:{} }
 
@@ -24,6 +25,12 @@ $(document).ready(function() {
         return number
     }
 
+    $window.resize(function() {
+        resize();
+    });
+  /* ----  SLIDER TILE GLOBAL END ---- */
+
+  /* ----  CASES STUDIES TILE START ---- */
     $('.flexslider-tile').each(function(){
       var limit = $(this).attr('data-slider');
       $(this).children('.flexslider-slider').flexslider({
@@ -35,22 +42,17 @@ $(document).ready(function() {
         itemMargin: 10,
         minItems: getGridSize(limit),
         maxItems: getGridSize(limit),
-        start: function(slider){
-          $('body').removeClass('loading');
-        }
       });
     });
 
-    $window.resize(function() {
-        resize();
-    });
-
     function resize() {
-        var limit = $('.flexslider-tile').attr('data-slider');
+      $('.flexslider-tile').each(function(){
+        var limit = $(this).attr('data-slider');
         var gridSize = getGridSize(limit);
 
-        $('.flexslider').data('flexslider').vars.minItems = gridSize;
-        $('.flexslider').data('flexslider').vars.maxItems = gridSize;
+        $(this).children('.flexslider').data('flexslider').vars.minItems = gridSize;
+        $(this).children('.flexslider').data('flexslider').vars.maxItems = gridSize;
+      });
     }
-  /* ----  SLIDER HOSTING TILE END ---- */
+  /* ----  CASES STUDIES TILE END ---- */
 });
