373dae50260f80bb9e9eb8e213f2cd024f868ccd
AuroreMariscal <aurore@affinitic.be>
2019-05-09 12:00:42 +0200

    Add banner to install and add style

--- src/renocopro/theme/setuphandlers.py
+++ src/renocopro/theme/setuphandlers.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from Products.CMFPlone.interfaces import INonInstallable
+from Products.CMFPlone.interfaces.controlpanel import IImagingSchema
 from collective.behavior.banner.banner import IBanner
 from plone import api
 from plone.app.imagecropping.behaviors import IImageCroppingBehavior
 from plone.dexterity.interfaces import IDexterityFTI
 from plone.namedfile.file import NamedBlobImage
+from plone.registry.interfaces import IRegistry
+from zope.component import queryUtility
 from zope.component import queryUtility
 from zope.interface import implementer
 import os
@@ -36,6 +39,7 @@ def post_install(context):
     add_behavior('Document', IBanner.__identifier__)
     add_behavior('Folder', IImageCroppingBehavior.__identifier__)
     add_behavior('Document', IImageCroppingBehavior.__identifier__)
+    add_format_to_banner(context)
 
 
 def uninstall(context):
@@ -53,3 +57,29 @@ def add_behavior(type_name, behavior_name):
     if behavior_name not in behaviors:
         behaviors.append(behavior_name)
         fti._updateProperty('behaviors', tuple(behaviors))
+
+
+def add_format_to_banner(context):
+    registry = queryUtility(IRegistry)
+    if not registry:
+        return None
+    settings = registry.forInterface(IImagingSchema, prefix="plone", check=False)
+    if not settings.allowed_sizes:
+        return None
+    settings.allowed_sizes.append(u'banner 1200:360')
+    api.portal.set_registry_record(
+        'collective.behavior.banner.browser.controlpanel.IBannerSettingsSchema.banner_scale',
+        u'banner'
+    )
+    api.portal.set_registry_record(
+        'collective.behavior.banner.browser.controlpanel.IBannerSettingsSchema.banner_scale',
+        u'banner'
+    )
+    api.portal.set_registry_record(
+        'plone.app.imagecropping.browser.settings.ISettings.cropping_for',
+        ["banner"]
+    )
+    api.portal.set_registry_record(
+        'plone.app.imagecropping.browser.settings.ISettings.constrain_cropping',
+        True
+    )
