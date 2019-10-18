fb174c840b972d6b05f605da289ad71ac896c098
Piret Valentin <valentin.piret@affinitic.be>
2019-10-18 10:37:12 +0200

    Back to development: 1.0a13

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='renocopro.theme',
-    version='1.0a12',
+    version='1.0a13.dev0',
     description="A theme add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
